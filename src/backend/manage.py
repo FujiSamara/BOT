2663848ebad4c5d8038cb2e76859208e4c23bc99
infanasotku <infanasotku@gmail.com>
2024-04-08 01:13:45 +0700

    Changed faztapi logging settings

--- src/backend/manage.py
+++ src/backend/manage.py
@@ -1,12 +1,15 @@
-from core.main import create_app
 import uvicorn
 from configure import configure
 from settings import get_settings
+from core.main import create_app
+
 
 def run():
     configure()
     get_settings()
-    uvicorn.run(create_app(), port=5000)
+    log_config = uvicorn.config.LOGGING_CONFIG
+    log_config["formatters"]["access"]["fmt"] = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+    uvicorn.run(create_app(), port=5000, log_config=log_config)
 
 if __name__ == "__main__":
     run()
