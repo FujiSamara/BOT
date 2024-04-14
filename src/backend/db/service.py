8e3f6f18db53493c1a5c92c9a890dd8f6ca95fe1
infanasotku <infanasotku@gmail.com>
2024-04-14 15:09:37 +0700

    Started work with crud

---
+++ src/backend/db/service.py
@@ -0,0 +1,26 @@
+
+def get_user_level_by_tg_id(id: str) -> int:
+    '''
+    Returns user access level by his telegram id.
+
+    Return -1, if user doesn't exits.
+    '''
+    # TODO: Completes calling orm.
+    return -1
+
+def get_user_role_by_tg_id(id: str) -> str:
+    '''
+    Return user role by his telegram id.
+
+    Return rule if user exist, undefined otherwise.
+    '''
+    pass
+
+def set_user_tg_id_by_number(number: str) -> bool:
+    '''
+    Finds user by his phone number and sets him telegram id.
+    
+    Returns True, if user found, False otherwise.
+    '''
+    # TODO: Completes func.
+    return True
