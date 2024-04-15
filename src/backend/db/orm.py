2b3e06e7ad1bc9f8c84637cab375bc832cc79cd7
infanasotku <infanasotku@gmail.com>
2024-04-15 21:56:06 +0700

    Started work with crud

--- src/backend/db/orm.py
+++ src/backend/db/orm.py
@@ -1,18 +1,41 @@
 from db.database import Base, engine, session
 from db.models import *
+from db.shemas import *
 
 def create_tables():
     Base.metadata.create_all(engine)
 
 
-def find_user_by_telegram_id(id: int) -> Worker:
-    '''Returns user if he exist, undefined otherwise.
+def find_worker_by_telegram_id(id: int) -> WorkerShema:
+    '''Returns user if he exist, `None` otherwise.
     '''
     with session.begin() as s:
-        return s.query(Worker).filter(Worker.telegram_id == id).first()
+        worker = s.query(Worker).filter(Worker.telegram_id == id).first()
+        if worker:
+            return WorkerShema.model_validate(worker)
+        return None
 
-def find_user_by_number(number: str) -> Worker:
-    '''Returns user if he exist, undefined otherwise.
+def find_worker_by_number(number: str) -> WorkerShema:
+    '''Returns user if he exist, `None` otherwise.
     '''
     with session.begin() as s:
-        return s.query(Worker).filter(Worker.phone_number == number).first()
+        worker = s.query(Worker).filter(Worker.phone_number == number).first()
+        if worker:
+            return WorkerShema.model_validate(worker)
+        return None
+
+def update_worker(worker: WorkerShema):
+    '''Updates worker by his id.
+    '''
+    with session.begin() as s:
+        cur_worker = s.query(Worker).filter(Worker.id == worker.id).first()
+        if not cur_worker:
+            return None
+        cur_worker.b_date = worker.b_date
+        cur_worker.f_name = worker.f_name
+        cur_worker.l_name = worker.l_name
+        cur_worker.o_name = worker.o_name
+        cur_worker.phone_number = worker.phone_number
+        cur_worker.telegram_id = worker.telegram_id
+
+    
--- src/backend/db/service.py
+++ src/backend/db/service.py
@@ -1,9 +1,12 @@
+from db.orm import find_worker_by_number, find_worker_by_telegram_id, update_worker
+from db.shemas import *
+import logging
 
 def get_user_level_by_tg_id(id: str) -> int:
     '''
     Returns user access level by his telegram id.
 
-    Return -1, if user doesn't exits.
+    Return `-1`, if user doesn't exits.
     '''
     # TODO: Completes calling orm.
     return -1
@@ -12,15 +15,24 @@ def get_user_role_by_tg_id(id: str) -> str:
     '''
     Return user role by his telegram id.
 
-    Return rule if user exist, undefined otherwise.
+    Return role if user exist, `None` otherwise.
     '''
     pass
 
-def set_user_tg_id_by_number(number: str) -> bool:
+def update_user_tg_id_by_number(number: str, tg_id: int) -> bool:
     '''
     Finds user by his phone number and sets him telegram id.
     
-    Returns True, if user found, False otherwise.
+    Returns `True`, if user found, `False` otherwise.
     '''
-    # TODO: Completes func.
+    worker = find_worker_by_number(number)
+    if not worker:
+        return False
+
+    worker.telegram_id = tg_id
+    try:
+        update_worker(worker)
+    except Exception as e:
+        logging.error(f"update_worker error: {e}")
+        return False
     return True
