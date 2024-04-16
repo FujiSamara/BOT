43369132798efe131ea913c99d097a3f9fa1e11b
infanasotku <infanasotku@gmail.com>
2024-04-17 00:01:35 +0700

    Added base port

--- src/backend/manage.py
+++ src/backend/manage.py
@@ -8,7 +8,7 @@ def run():
     
     from core.main import create_app
     log_config_path = (pathlib.Path(__file__).parent / 'log_config.yaml').resolve().as_posix()
-    uvicorn.run(create_app(), host='0.0.0.0', port=5200, log_config=log_config_path)
+    uvicorn.run(create_app(), host='0.0.0.0', port=80, log_config=log_config_path)
 
 if __name__ == "__main__":
     run()
