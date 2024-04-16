013e7e54c2c0428856b04881695d3b8091b0e341
infanasotku <infanasotku@gmail.com>
2024-04-16 10:38:34 +0700

    Competed set amount form

--- src/backend/bot/handlers/bid_handler.py
+++ src/backend/bot/handlers/bid_handler.py
@@ -2,14 +2,15 @@ from aiogram import F, Router
 from aiogram.types import CallbackQuery, Message
 from aiogram.fsm.context import FSMContext
 from aiogram.utils.markdown import hbold
-from bot.bot import get_bot
 
 # bot imports
-from bot.kb import bid_menu, create_bid_menu, main_menu
+from bot.kb import bid_menu, create_bid_menu, main_menu, main_menu_button
 
 from bot.text import bid_amount_err
 
 from bot.states import BidCreating
+
+from bot.bot import get_bot
 
 # db imports
 
@@ -27,18 +28,16 @@ async def get_create_menu(callback: CallbackQuery):
 @router.callback_query(F.data == "get_amount_form")
 async def get_amount_form(callback: CallbackQuery, state: FSMContext):
     await state.set_state(BidCreating.payment_amount)
-    await state.update_data(menu_id=callback.message.message_id)
     await callback.message.edit_text(hbold("Введите требуемую сумму:"), reply_markup=main_menu)
 
 @router.message(BidCreating.payment_amount)
 async def set_amount(message: Message, state: FSMContext):
     try:
         amount = int(message.html_text)
+        await state.update_data(amount=amount)
+        await message.answer(hbold("Успешно!"))
+        await message.answer(hbold("Настройте вашу заявку:"), reply_markup=create_bid_menu)
     except:
-        pr_msg_id = (await state.get_data())["menu_id"]
-        await get_bot().delete_message(chat_id=message.chat.id, message_id=pr_msg_id)
         await message.answer(bid_amount_err, 
                                 reply_markup=main_menu)
-        return
-    print(amount)
-    await state.clear()
+        
