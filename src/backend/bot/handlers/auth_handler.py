a29e4a6f53d1876c47500a820e32ceacd9f200cb
infanasotku <infanasotku@gmail.com>
2024-04-16 15:36:26 +0700

    Corrected state

--- src/backend/bot/handlers/auth_handler.py
+++ src/backend/bot/handlers/auth_handler.py
@@ -5,7 +5,7 @@ from aiogram.fsm.context import FSMContext
 # bot imports
 from bot.text import user_not_exist_text
 
-from bot.states import Auth
+from bot.states import Auth, Base
 
 from bot.handlers.main_handler import send_menu_by_level
 
@@ -20,7 +20,7 @@ router = Router(name="auth")
 @flags.chat_action("typing")
 async def auth(message: Message, state: FSMContext):
     if update_user_tg_id_by_number(message.html_text, message.from_user.id):
-        await state.clear()
+        await state.set_data(Base.none)
         await send_menu_by_level(message)
     else:
         await message.answer(user_not_exist_text)
