a75d4147ffa95ad0e34f527de5ff21f5845c70f3
infanasotku <infanasotku@gmail.com>
2024-04-16 13:59:11 +0700

    Added payment type section

--- src/backend/bot/handlers/bid_handler.py
+++ src/backend/bot/handlers/bid_handler.py
@@ -4,13 +4,17 @@ from aiogram.fsm.context import FSMContext
 from aiogram.utils.markdown import hbold
 
 # bot imports
-from bot.kb import bid_menu, create_bid_menu, main_menu, main_menu_button
+from bot.kb import (
+    bid_menu,
+    create_bid_menu,
+    payment_type_menu,
+    bid_menu_button,
+    create_inline_keyboard
+)
 
-from bot.text import bid_amount_err
+from bot.text import bid_err, payment_types
 
 from bot.states import BidCreating
-
-from bot.bot import get_bot
 
 # db imports
 
@@ -21,23 +25,35 @@ router = Router(name="bid")
 async def get_menu(callback: CallbackQuery):
     await callback.message.edit_text(hbold("Добро пожаловать!"), reply_markup=bid_menu)
 
+# Create bid section
 @router.callback_query(F.data == "create_bid")
 async def get_create_menu(callback: CallbackQuery):
     await callback.message.edit_text(hbold("Настройте вашу заявку:"), reply_markup=create_bid_menu)
 
+# Amount section
 @router.callback_query(F.data == "get_amount_form")
 async def get_amount_form(callback: CallbackQuery, state: FSMContext):
     await state.set_state(BidCreating.payment_amount)
-    await callback.message.edit_text(hbold("Введите требуемую сумму:"), reply_markup=main_menu)
+    await callback.message.edit_text(hbold("Введите требуемую сумму:"),
+                                     reply_markup=create_inline_keyboard(bid_menu_button))
 
 @router.message(BidCreating.payment_amount)
 async def set_amount(message: Message, state: FSMContext):
     try:
         amount = int(message.html_text)
         await state.update_data(amount=amount)
-        await message.answer(hbold("Успешно!"))
-        await message.answer(hbold("Настройте вашу заявку:"), reply_markup=create_bid_menu)
+        await message.answer(hbold("Успешно! Настройте вашу заявку:"), reply_markup=create_bid_menu)
     except:
-        await message.answer(bid_amount_err, 
-                                reply_markup=main_menu)
+        await message.answer(bid_err, 
+                                reply_markup=create_inline_keyboard(bid_menu_button))
         
+# Payment type section
+@router.callback_query(F.data == "set_paymant_type")
+async def get_amoun_form(callback: CallbackQuery, state: FSMContext):
+    await callback.message.edit_text(hbold("Выберите тип оплаты:"), reply_markup=payment_type_menu)
+
+@router.callback_query(F.data.in_(payment_types))
+async def set_type(callback: CallbackQuery, state: FSMContext):
+    if callback.data in payment_types:
+        await state.update_data(type=callback.data)
+        await callback.message.edit_text(hbold("Успешно! Настройте вашу заявку:"), reply_markup=create_bid_menu)
--- src/backend/bot/kb.py
+++ src/backend/bot/kb.py
@@ -12,10 +12,6 @@ bid_menu_button = InlineKeyboardButton(text="📝 Меню создания за
 main_menu_button = InlineKeyboardButton(text="🚪 Главное меню", callback_data="get_menu")
 
 # Keyboards
-main_menu = InlineKeyboardMarkup(inline_keyboard=[
-    [main_menu_button]
-])
-
 bid_menu = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="📝 Создать заявку", callback_data="create_bid")],
     [InlineKeyboardButton(text="🧭 Ожидающие заявки", callback_data="get_pending_bid")],
@@ -29,5 +25,17 @@ create_bid_menu = InlineKeyboardMarkup(inline_keyboard=[
     # TODO: Sets remaining payment button
     #[InlineKeyboardButton(text="🕰 История заявок", callback_data="get_history_bid")],
 
-    [main_menu_button],
+    [bid_menu_button],
 ])
+
+payment_type_menu = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="💵 Наличная", callback_data="cash"),
+    InlineKeyboardButton(text="💳 Безналичная", callback_data="card")],
+    [InlineKeyboardButton(text="🚕 Требуется такси", callback_data="taxi")],
+    [bid_menu_button]
+])
+
+def create_inline_keyboard(*buttons):
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [button for button in buttons]
+    ])
