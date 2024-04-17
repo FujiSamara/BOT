037449d2b948e4df972cd781ec6459072dda325d
infanasotku <infanasotku@gmail.com>
2024-04-17 22:31:07 +0700

    Added new settings

--- src/backend/manage.py
+++ src/backend/manage.py
@@ -1,14 +1,22 @@
 import uvicorn
 from configure import configure
 import pathlib
+from core.main import create_app
+from settings import get_settings
 
 
 def run():
     configure()
     
-    from core.main import create_app
     log_config_path = (pathlib.Path(__file__).parent / 'log_config.yaml').resolve().as_posix()
-    uvicorn.run(create_app(), host='0.0.0.0', port=80, log_config=log_config_path)
+    uvicorn.run(
+        app=create_app(), 
+        host=get_settings().host, 
+        port=get_settings().port, 
+        log_config=log_config_path,
+        ssl_keyfile=get_settings().ssl_keyfile,
+        ssl_certfile=get_settings().ssl_certfile
+    )
 
 if __name__ == "__main__":
     run()
