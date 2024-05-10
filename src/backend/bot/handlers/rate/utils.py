c30593872ae9ad66bfbe7073774aed21f4611dd6
infanasotku <infanasotku@gmail.com>
2024-05-10 23:33:22 +0700

    Added getting shift status

--- src/backend/bot/handlers/rate/utils.py
+++ src/backend/bot/handlers/rate/utils.py
@@ -1,4 +1,6 @@
 from datetime import date
+from db import service
+from settings import get_settings
 
 
 def get_shift_status(day: date, department_id: int) -> bool:
@@ -7,5 +9,12 @@ def get_shift_status(day: date, department_id: int) -> bool:
 
     Returns `True` if shift closed `False` otherwise.
     """
-    # TODO: Make checking status.
-    return False
+    records = service.get_work_time_records_by_day_and_department(
+        department_id, day.strftime(get_settings().date_format)
+    )
+
+    for record in records:
+        if not record.fine and not record.rating:
+            return False
+
+    return True
