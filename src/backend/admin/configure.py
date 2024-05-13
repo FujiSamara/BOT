bd3da6b5778a5d6863ea7b0b580b3e5fc5ca9e61
infanasotku <infanasotku@gmail.com>
2024-05-13 19:44:42 +0700

    Fixed shift view

--- src/backend/admin/configure.py
+++ src/backend/admin/configure.py
@@ -4,7 +4,7 @@ from admin.schemas import (
     PostView,
     WorkerView,
     BidView,
-    WorkerBidView
+    WorkerBidView,
 )
 
 
@@ -13,8 +13,7 @@ from admin.admin import FujiAdmin
 
 
 def configure(admin: FujiAdmin):
-    '''Configure fast api admin app.
-    '''
+    """Configure fast api admin app."""
     admin.add_view(DepartmentView)
     admin.add_view(CompanyView)
     admin.add_view(PostView)
