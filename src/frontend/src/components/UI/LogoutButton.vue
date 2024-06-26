5922b98a33c283c6b9223ec24662563c33d70095
infanasotku <infanasotku@gmail.com>
2024-06-26 22:42:32 +0700

    Completed logout button

---
+++ src/frontend/src/components/UI/LogoutButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="button-wrapper" @click="$emit('click')">
+        <img src="/img/logout-logo.svg">
+        <p>выход</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "logout-button",
+    emits: ['click']
+}
+</script>
+<style scoped>
+.button-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 2px;
+}
+.button-wrapper p {
+    font-size: 18px;
+    font-family: Stolzl;
+    font-weight: 400;
+    margin: 0;
+    color: #FF0038;
+}
+.button-wrapper img {
+    width: 44px;
+    height: 44px;
+}
+</style>
