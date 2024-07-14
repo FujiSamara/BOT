b28b0237f2ccc9f5fa0dad4099ec160a8d86a860
infanasotku <infanasotku@gmail.com>
2024-07-14 21:31:17 +0700

    Started work with editing page

---
+++ src/frontend/src/components/EditPanelElement.vue
@@ -0,0 +1,13 @@
+<template>
+	<div classs="edit-panel-wrapper"></div>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+.edit-panel-wrapper {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 14px 10px 14px 20px;
+	background-color: #ffffff;
+}
+</style>
