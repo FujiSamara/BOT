a03b610352a55178d1d138209ad12cf8f05686e3
infanasotku <infanasotku@gmail.com>
2024-07-20 13:25:34 +0700

    Added cors

--- src/backend/core/configure.py
+++ src/backend/core/configure.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
+from core.middlewares.setup import setup_core_middlewares
+
 # Apps
 import admin
 import bot
@@ -9,9 +11,11 @@ import api
 
 def configure(app: FastAPI):
     """Configures fast api core app."""
-    # bot.create(app)
+    bot.create(app)
     admin.create(app)
     api.create(app)
+
+    setup_core_middlewares(app)
 
 
 @asynccontextmanager
