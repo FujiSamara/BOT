7ab83ba3c4494233c547687b50bca5fa38163548
infanasotku <infanasotku@gmail.com>
2024-07-21 17:34:18 +0700

    Added worker field

--- src/frontend/src/editor.ts
+++ src/frontend/src/editor.ts
@@ -1,6 +1,8 @@
+import axios from "axios";
 import { computed, Ref, ref } from "vue";
+import * as config from "@/config";
 
-export class SmartField {
+class SmartField {
 	protected _rawField: Ref<any> = ref();
 	protected _tipList: Ref<Array<any>> = ref([]);
 
@@ -14,16 +16,19 @@ export class SmartField {
 		}
 	}
 
-	protected _formatter: (value: any) => string = (value) => `${value}`;
-	protected _setter: (newValue: any) => void = (newValue: any) =>
-		(this._rawField.value = newValue);
+	protected formatter(value: any): string {
+		return `${value}`;
+	}
+	protected async setter(newValue: any): Promise<void> {
+		this._rawField.value = newValue;
+	}
 
 	public formattedField = computed({
 		get: () => {
-			return this._formatter(this._rawField.value);
+			return this.formatter(this._rawField.value);
 		},
-		set: (newValue: string) => {
-			this._setter(newValue);
+		set: async (newValue: string) => {
+			await this.setter(newValue);
 		},
 	});
 
@@ -32,7 +37,7 @@ export class SmartField {
 	}
 
 	public selectList = computed(() => {
-		return this._tipList.value.map((value) => this._formatter(value));
+		return this._tipList.value.map((value) => this.formatter(value));
 	});
 
 	public applySelection(index: number) {
@@ -67,7 +72,36 @@ export class ExpenditureEditor extends Editor {
 				new SmartField("Статья", "name", _instance.name),
 				new SmartField("Раздел", "chapter", _instance.chapter),
 				new SmartField("Лимит", "limit", _instance.limit),
+				new WorkerSmartField("ЦФО", "fac", _instance.fac),
+				new WorkerSmartField("ЦЗ", "cc", _instance.cc),
+				new WorkerSmartField(
+					"Руководитель ЦЗ",
+					"cc_supervisor",
+					_instance.cc_supervisor,
+				),
 			];
 		}
+	}
+}
+
+class WorkerSmartField extends SmartField {
+	private _endpoint: string = "";
+
+	constructor(name: string, fieldName: string, defaultValue?: any) {
+		super(name, fieldName, defaultValue);
+		this._endpoint = `http://${config.backendDomain}:${config.backendPort}/${config.crmEndpoint}/worker`;
+	}
+
+	protected formatter(value: any): string {
+		return `${value.l_name} ${value.f_name} ${value.o_name}`;
+	}
+	protected async setter(newValue: any): Promise<void> {
+		const resp = await axios.get(`${this._endpoint}/find?record=${newValue}`);
+		if (resp.status >= 400) {
+			throw Error("Bad value");
+		}
+
+		this._tipList.value = resp.data;
+		console.log(this._tipList.value);
 	}
 }
