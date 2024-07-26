8fd4728a709c57d4ad5c60c3c390b9b8e60a5db3
infanasotku <infanasotku@gmail.com>
2024-07-26 18:57:39 +0700

    Added checking auth

--- src/backend/api/auth/configure.py
+++ src/backend/api/auth/configure.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Security
 from datetime import timedelta
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
@@ -8,11 +8,19 @@ from api.auth.authentication import _authenticate_user, _create_access_token
 
 from settings import get_settings
 
+from api.auth.schemas import User
+from api.auth.authentication import get_current_user
+
 
 def configure(auth: FastAPI):
     """Configure fast api auth app."""
 
     auth.post("/token", response_model=Token)(login)
+    auth.get("/")(check_auth)
+
+
+async def check_auth(_: User = Security(get_current_user, scopes=["authenticated"])):
+    return
 
 
 async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
