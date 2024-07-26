1a19f84a63e712095f55448c1fafe2395edb2d51
infanasotku <infanasotku@gmail.com>
2024-07-26 19:41:55 +0700

    Added security endpoint to worker

--- src/backend/api/crm/routers/worker.py
+++ src/backend/api/crm/routers/worker.py
@@ -1,14 +1,19 @@
+from fastapi import Security
 from fastapi.routing import APIRouter
 
 from db import service
 from db.schemas import WorkerSchema
+
+from api.auth import User, get_current_user
 
 
 router = APIRouter()
 
 
 @router.get("/find")
-async def find_workers(record: str) -> list[WorkerSchema]:
+async def find_workers(
+    record: str, _: User = Security(get_current_user, scopes=["authenticated"])
+) -> list[WorkerSchema]:
     """Finds workers by given `record`.
 
     Search is carried out by f_name, l_name, o_name.
