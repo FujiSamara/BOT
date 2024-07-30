e7cd0d2a4e0b21352db7ea340dd9b9f1cc99ac21
infanasotku <infanasotku@gmail.com>
2024-07-30 14:13:28 +0700

    Modifed cors settings

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -20,9 +20,15 @@ class Settings(BaseSettings):
     # endregion
 
     # region Security
-    cors_origins: list[str] = [
-        "http://localhost:5001",
-    ]
+    cors_origins_str: str = Field(
+        validation_alias="CORS_ORIGINS", default="http://localhost:5001"
+    )
+
+    @computed_field
+    @property
+    def cors_origins(self) -> list[str]:
+        return self.cors_origins_str.split()
+
     cors_allow_methods: list[str] = ["GET", "POST", "PATCH", "PUT", "DELETE"]
     cors_allow_headers: list[str] = [
         "Content-Type",
