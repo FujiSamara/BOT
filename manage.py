215900c7d0275fa48b521786e7f93c2ab79f5ef2
infanasotku <infanasotku@gmail.com>
2024-07-31 17:11:48 +0700

    Corrected comments

--- manage.py
+++ manage.py
@@ -9,15 +9,27 @@ from src.server import tunnel
 CURRENT_DIRECTORY = pathlib.Path(__file__).parent.resolve()
 EXECUTEABLE_PATH = sys.executable
 
+OKBLUE = "\033[94m"
+ENDC = "\033[0m"
+
+
+class AppProcess:
+    def __init__(self, name: str, args: list[str]):
+        self.name = name
+        self.args = args
+
+
+class AppTask:
+    def __init__(self, name: str, task: Callable):
+        self.name = name
+        self.callback = task
+
 
 def run():
-    OKBLUE = "\033[94m"
-    ENDC = "\033[0m"
-
-    # region Configuring main commands
+    # region Configuring main processes
     print(OKBLUE + "[Processes configuring started]" + ENDC)
 
-    processes: list[list] = []
+    processes: list[AppProcess] = []
 
     # Backend
     backend_cmd = [
@@ -26,7 +38,7 @@ def run():
         f"{CURRENT_DIRECTORY}/src/backend/manage.py",
     ]
     if "FRONTEND" not in sys.argv:
-        processes.append(backend_cmd)
+        processes.append(AppProcess("Backend", backend_cmd))
 
     # Frontend
     frontend_cmd = [
@@ -37,46 +49,52 @@ def run():
         "dev",
     ]
     if "BACKEND" not in sys.argv:
-        processes.append(frontend_cmd)
-
-    procs: list[sub.Popen] = []
+        processes.append(AppProcess("Frontend", frontend_cmd))
 
     print(OKBLUE + "[Processes configuring completed]" + ENDC)
     # endregion
 
-    # region Configuring and launch preLaunch tasks
-    print(OKBLUE + "[Prelaunch tasks started]" + ENDC)
-
-    pre_launch_tasks = []
+    # Prelaunch tasks
+    pre_launch_tasks: list[AppTask] = []
 
     if "TUNNEL" in sys.argv:
-        pre_launch_tasks.append(tunnel.run)
+        pre_launch_tasks.append(AppTask("Tunnel", tunnel.run))
 
     run_pre_launch_tasks(pre_launch_tasks)
 
-    print(OKBLUE + "[Prelaunch tasks completed]" + ENDC)
-    # endregion
+    # Run processes
+    run_processes(processes)
 
-    # region Launch and waiting processes.
-    print(OKBLUE + "[Processes started]" + ENDC)
+
+def run_pre_launch_tasks(tasks: list[AppTask]):
+    print(OKBLUE + "[Prelaunch tasks starting]" + ENDC)
+    try:
+        for task in tasks:
+            print(OKBLUE + f"[Task({task.name}) starting]" + ENDC)
+            task.callback()
+            print(OKBLUE + f"[Task({task.name}) started]" + ENDC)
+    except Exception as e:
+        print(e)
+        sys.exit()
+
+    print(OKBLUE + "[Prelaunch tasks completed]" + ENDC)
+
+
+def run_processes(processes: list[AppProcess]):
+    print(OKBLUE + "[Processes starting]" + ENDC)
+    procs: list[sub.Popen] = []
 
     for cmd in processes:
-        proc = sub.Popen(args=cmd)
+        print(OKBLUE + f"[Process({cmd.name}) starting]" + ENDC)
+        proc = sub.Popen(args=cmd.args)
+        print(OKBLUE + f"[Process({cmd.name}) started]" + ENDC)
         procs.append(proc)
+    print(OKBLUE + "[Processes started]" + ENDC)
+
     for proc in procs:
         proc.wait()
 
     print(OKBLUE + "[Processes completed]" + ENDC)
-    # endregion
-
-
-def run_pre_launch_tasks(tasks: list[Callable]):
-    try:
-        for task in tasks:
-            task()
-    except Exception as e:
-        print(e)
-        sys.exit()
 
 
 if __name__ == "__main__":
