d8d9c3d2deb562baf0269b906b1589199bedbd25
infanasotku <infanasotku@gmail.com>
2024-08-05 19:45:30 +0700

    Deleted extra

--- src/backend/api/auth/permissions.py
+++ src/backend/api/auth/permissions.py
@@ -8,14 +8,6 @@ _scopes = {
     "crm_bid": "Can view bid crm page",
     "crm_budget": "Can view budget crm page",
     "crm_expenditure": "Can view expenditure crm page",
-    # BOT
-    "bot_bid_create": "Can view bid bot creating form",
-    "bot_bid_kru": "Can view bid bot kru form",
-    "bot_bid_owner": "Can view bid bot owner form",
-    "bot_bid_teller_cash": "Can view bid bot teller cash form",
-    "bot_bid_teller_card": "Can view bid bot teller card form",
-    "bot_bid_accountant_cash": "Can view bid bot accountant cash form",
-    "bot_bid_accountant_card": "Can view bid bot accountant card form",
 }
 
 
