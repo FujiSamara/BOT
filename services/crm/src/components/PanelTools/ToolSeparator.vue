5f1afa5f6a98d0aa34f9423591c47a277508dd37
infanasotku <infanasotku@gmail.com>
2024-10-01 13:53:32 +0700

    Fixed tool separator

--- services/crm/src/components/PanelTools/ToolSeparator.vue
+++ services/crm/src/components/PanelTools/ToolSeparator.vue
@@ -1,10 +1,12 @@
 <template>
-	<div></div>
+	<span>|</span>
 </template>
 <style scoped>
-div {
+span {
+	font-size: 33px;
 	height: 33px;
 	width: 1px;
 	background-color: #d7dee3;
+	color: transparent;
 }
 </style>
