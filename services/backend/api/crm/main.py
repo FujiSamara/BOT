44b64fd6a3f425d5c211ad3e9ceaf601c5b1ceae
infanasotku <infanasotku@gmail.com>
2024-10-22 14:36:50 +0700

    Deleted extra

--- services/backend/api/crm/main.py
+++ services/backend/api/crm/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-from api.crm.configure import configure
 import logging
 import sys
+
+from api.crm.routers import expenditure
+from api.crm.routers import worker
+from api.crm.routers import budget
+from api.crm.routers import department
+from api.crm.routers import bid
+from api.crm.routers import worktime
+from api.crm.routers import post
 
 
 def create(api: FastAPI) -> FastAPI:
@@ -14,3 +21,14 @@ def create(api: FastAPI) -> FastAPI:
     logging.info("Crm created")
     api.mount(path="/crm", app=crm)
     return crm
+
+
+def configure(crm: FastAPI):
+    """Configure fast api crm app."""
+    crm.include_router(expenditure.router, prefix="/expenditure")
+    crm.include_router(worker.router, prefix="/worker")
+    crm.include_router(budget.router, prefix="/budget")
+    crm.include_router(department.router, prefix="/department")
+    crm.include_router(bid.router, prefix="/bid")
+    crm.include_router(worktime.router, prefix="/worktime")
+    crm.include_router(post.router, prefix="/post")
