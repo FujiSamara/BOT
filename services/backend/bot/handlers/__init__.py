3a4c323a6dddb4282df75fbdb7d25271ef9225a8
infanasotku <infanasotku@gmail.com>
2024-10-25 18:09:06 +0700

    Corrected import

--- services/backend/bot/handlers/__init__.py
+++ services/backend/bot/handlers/__init__.py
@@ -1,9 +1,9 @@
-from bids.main import router as bid_router
-from bids_it.main import router as bids_it_router
-from monitoring.main import router as monitoring_router
-from perconal_cab.main import router as personal_cabinet_router
-from rate.main import router as rate_router
-from tech_request.main import router as tech_request_router
+from bot.handlers.bids.main import router as bid_router
+from bot.handlers.bids_it.main import router as bids_it_router
+from bot.handlers.monitoring.main import router as monitoring_router
+from bot.handlers.perconal_cab.main import router as personal_cabinet_router
+from bot.handlers.rate.main import router as rate_router
+from bot.handlers.tech_request.main import router as tech_request_router
 
 __all__ = [
     "bid_router",
