3ec779a8729369c6dbc456921d2afdb66e9d1a36
infanasotku <infanasotku@gmail.com>
2024-11-27 21:38:55 +0700

    Corrected tunnel

--- services/server/tunnel.py
+++ services/server/tunnel.py
@@ -1,4 +1,6 @@
+import time
 import ngrok
+import asyncio
 from dotenv import load_dotenv, find_dotenv, set_key, get_key
 
 
@@ -14,6 +16,13 @@ def run():
     ngrok.log_level("ERROR")
     listener = ngrok.forward(f"{domain}:{port}", authtoken_from_env=True)
     set_key(dotenv_path, "BOT_WEBHOOK_URL", f"{listener.url()}/bot/webhook")
+
+    try:
+        while True:
+            time.sleep(1)  # Удерживаем программу активной
+    except KeyboardInterrupt:
+        print("Shutting down tunnel...")
+        asyncio.run(listener.close())
 
 
 if __name__ == "__main__":
