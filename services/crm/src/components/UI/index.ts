adb3254bf9762cfd2a6c9dd981086aaa53e79a2c
infanasotku <infanasotku@gmail.com>
2024-12-02 17:49:33 +0700

    Deleted extra  components

--- services/crm/src/components/UI/index.ts
+++ services/crm/src/components/UI/index.ts
@@ -2,9 +2,6 @@ import WindowCross from "@/components/UI/WindowCross.vue";
 import BorderInput from "@/components/UI/BorderInput.vue";
 import PurpleButton from "@/components/UI/PurpleButton.vue";
 import NamedButton from "@/components/UI/NamedButton.vue";
-import LogoutButton from "@/components/UI/LogoutButton.vue";
-import TableCheckbox from "@/components/UI/TableCheckbox.vue";
-import ClickableIcon from "@/components/UI/ClickableIcon.vue";
 import SelectList from "@/components/UI/SelectList.vue";
 import CircleLoader from "@/components/UI/CircleLoader.vue";
 import ChooseFiles from "@/components/UI/ChooseFiles.vue";
@@ -14,9 +11,6 @@ export default [
 	BorderInput,
 	PurpleButton,
 	NamedButton,
-	LogoutButton,
-	TableCheckbox,
-	ClickableIcon,
 	SelectList,
 	CircleLoader,
 	ChooseFiles,
