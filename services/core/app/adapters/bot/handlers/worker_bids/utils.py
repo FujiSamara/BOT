c81712b2ef91b1106c69133772816832a4c5f0a9
Ogyrec225 <kasimovskyvladislav@yandex.ru>
2024-12-16 13:19:57 +0400

    Fix merge bugs

--- services/core/app/adapters/bot/handlers/worker_bids/utils.py
+++ services/core/app/adapters/bot/handlers/worker_bids/utils.py
@@ -1,4 +1,4 @@
-from app.infra.database.schemas import WorkerBidSchema
+from app.schemas import WorkerBidSchema
 from app.infra.database.models import ApprovalStatus
 from app.infra.config import settings
 from aiogram.utils.markdown import hbold
