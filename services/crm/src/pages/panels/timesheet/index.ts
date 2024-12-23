9d495bf26bcd8a2f94bab2f0051f95be9463a08e
infanasotku <infanasotku@gmail.com>
2024-12-23 21:59:48 +0700

    add id field

--- services/crm/src/pages/panels/timesheet/index.ts
+++ services/crm/src/pages/panels/timesheet/index.ts
@@ -5,10 +5,12 @@ export class TimesheetTable extends Table<TimesheetSchema> {
 	constructor() {
 		super("timesheet");
 
+		this._aliases.set("id", "Айди работника");
 		this._aliases.set("worker_fullname", "Работник");
 		this._aliases.set("post_name", "Должность");
 		this._aliases.set("total_hours", "Всего отработано");
 
+		this._columsOrder.set("id", 0);
 		this._columsOrder.set("worker_fullname", 1);
 		this._columsOrder.set("post_name", 2);
 		this._columsOrder.set("total_hours", 2);
