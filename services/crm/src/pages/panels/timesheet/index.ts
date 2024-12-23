710162e63064069772a0d91ce23515252eac875d
infanasotku <infanasotku@gmail.com>
2024-12-23 21:51:44 +0700

    Add order of columens in timesheet panel

--- services/crm/src/pages/panels/timesheet/index.ts
+++ services/crm/src/pages/panels/timesheet/index.ts
@@ -8,5 +8,9 @@ export class TimesheetTable extends Table<TimesheetSchema> {
 		this._aliases.set("worker_fullname", "Работник");
 		this._aliases.set("post_name", "Должность");
 		this._aliases.set("total_hours", "Всего отработано");
+
+		this._columsOrder.set("worker_fullname", 1);
+		this._columsOrder.set("post_name", 2);
+		this._columsOrder.set("total_hours", 2);
 	}
 }
