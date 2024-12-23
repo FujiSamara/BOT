e3db7d35eb0f03ec37036e59944fe2db6af9ca08
infanasotku <infanasotku@gmail.com>
2024-12-23 19:22:07 +0700

    Add calendar to date filter

--- services/crm/src/components/table/tools/DateFilter.vue
+++ services/crm/src/components/table/tools/DateFilter.vue
@@ -1,25 +1,81 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, onUnmounted, PropType, ref } from "vue";
+import { CalendarType, DateType } from "@/types";
+import DateCalendar from "@/components/DateCalendar.vue";
 
-const modes = ["Промежуток"];
-const currentMode = ref(0);
+const props = defineProps({
+	mode: {
+		type: String as PropType<DateType>,
+		default: DateType.Month,
+	},
+});
+
+const calendarVisible = ref(true);
+
+const from = ref(new Date());
+const to = ref(new Date());
+
+const monthChanged = (date: Date) => {
+	calendarVisible.value = false;
+	setIntervalFromDay(date);
+};
+// TODO: Complete for day and year
+
+const setIntervalFromDay = (date: Date) => {
+	from.value = new Date(date.getFullYear(), date.getMonth(), 1, 0, 0, 0, 0);
+	to.value = new Date(date.getFullYear(), date.getMonth() + 1, 1, 0, 0, 0, 0);
+};
+
+const calendarOutsideClicked = () => {
+	calendarVisible.value = false;
+};
+
+onMounted(() => {
+	setIntervalFromDay(new Date());
+
+	document.addEventListener("click", calendarOutsideClicked);
+});
+
+onUnmounted(() => {
+	document.removeEventListener("click", calendarOutsideClicked);
+});
 </script>
 
 <template>
 	<div class="date-filter">
 		<div class="df-mode">
 			<div class="tool-icon-wrapper">
-				<div class="tool-icon calendar"></div>
+				<div class="tool-icon calendar-icon"></div>
 			</div>
-			<span class="df-title">{{ modes[currentMode] }}</span>
+			<span class="df-title">{{ props.mode.toString() }}</span>
 		</div>
+
+		<!-- Modes -->
 		<Transition name="fade">
-			<div v-if="modes[currentMode] === 'Промежуток'" class="df-input">
+			<div v-if="props.mode === DateType.Interval" class="df-value">
 				<span>с</span>
 				<input placeholder="01.01.2024" />
 				<span>по</span>
 				<input placeholder="05.01.2024" />
 			</div>
+		</Transition>
+		<Transition name="fade">
+			<div v-if="props.mode === DateType.Month" class="df-value">
+				<span @click.stop="calendarVisible = !calendarVisible">{{
+					from.toLocaleString("ru", { month: "long" })
+				}}</span>
+			</div>
+		</Transition>
+
+		<Transition name="fade">
+			<DateCalendar
+				v-if="calendarVisible"
+				class="calendar"
+				:lock-mode="CalendarType.Month"
+				:date="from"
+				@submit="monthChanged"
+				@click.stop
+			></DateCalendar>
 		</Transition>
 	</div>
 </template>
@@ -47,7 +103,7 @@ const currentMode = ref(0);
 			height: 16px;
 
 			.tool-icon {
-				&.calendar {
+				&.calendar-icon {
 					width: 16px;
 					height: 16px;
 					color: $fuji-white;
@@ -62,7 +118,7 @@ const currentMode = ref(0);
 		}
 	}
 
-	.df-input {
+	.df-value {
 		display: flex;
 		flex-direction: row;
 		align-items: center;
@@ -70,23 +126,37 @@ const currentMode = ref(0);
 		gap: 4px;
 
 		transform: opacity 1s;
+		cursor: pointer;
 
-		input {
+		span {
+			display: flex;
+			justify-content: center;
+			align-items: center;
 			width: 97px;
 			height: 26px;
-
-			border: none;
-			outline: none;
 
 			padding: 4px 12px;
 			border-radius: 8px;
 			background-color: $fuji-white-darker;
 
-			&::placeholder {
-				font-size: 14px;
-				color: $text-color;
-			}
+			font-family: Wix Madefor Display;
+			font-weight: 500;
+			font-size: 14px;
+			color: $text-color;
+
+			text-transform: capitalize;
 		}
+	}
+
+	.calendar {
+		position: absolute;
+
+		top: 100%;
+		left: 0;
+	}
+
+	&:hover {
+		border-color: $border-color;
 	}
 }
 </style>
