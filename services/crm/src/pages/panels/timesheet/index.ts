93e4f5fecba64f33c8ee4864de8de9fe3b05a892
infanasotku <infanasotku@gmail.com>
2024-12-25 14:55:18 +0700

    Forward department column

--- services/crm/src/pages/panels/timesheet/index.ts
+++ services/crm/src/pages/panels/timesheet/index.ts
@@ -10,15 +10,17 @@ export class TimesheetTable extends Table<TimesheetSchema> {
 	constructor() {
 		super("timesheet");
 
-		this._aliases.set("id", "ID работника");
+		this._aliases.set("id", "ID");
 		this._aliases.set("worker_fullname", "Работник");
 		this._aliases.set("post_name", "Должность");
+		this._aliases.set("department_name", "Предприятие");
 		this._aliases.set("total_hours", "Всего отработано");
 
 		this._columsOrder.set("id", 0);
 		this._columsOrder.set("worker_fullname", 1);
 		this._columsOrder.set("post_name", 2);
-		this._columsOrder.set("total_hours", 3);
+		this._columsOrder.set("department_name", 3);
+		this._columsOrder.set("total_hours", 4);
 	}
 
 	public orderDisabled(header: string): boolean {
