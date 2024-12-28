fbbbbcf273dab02655c51282ec310b057f9407b1
infanasotku <infanasotku@gmail.com>
2024-12-28 12:00:55 +0700

    Remov forced width

--- services/crm/src/pages/panels/expenditure/ExpenditurePanel.vue
+++ services/crm/src/pages/panels/expenditure/ExpenditurePanel.vue
@@ -72,7 +72,7 @@ const searchList = useSearch(
 			<div class="tb-outer-group">
 				<div class="tb-group">
 					<ColumnFilter
-						:style="'width: 126px; height: 48px'"
+						:style="'height: 48px'"
 						:table="props.table"
 						:alignRight="true"
 					></ColumnFilter>
