17a3fa54cca15c10d80665bad1b37b63f491f537
infanasotku <infanasotku@gmail.com>
2025-01-27 22:53:39 +0700

    Add blur modal with blur backgournd

---
+++ services/crm/src/components/BlurModal.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted } from "vue";
+
+const emits = defineEmits(["close"]);
+
+const keyDown = (e: KeyboardEvent) => {
+	if (e.key === "Escape") {
+		emits("close");
+	}
+};
+
+onMounted(() => {
+	document.addEventListener("keydown", keyDown);
+});
+
+onUnmounted(() => {
+	document.removeEventListener("keydown", keyDown);
+});
+</script>
+
+<template>
+	<div class="bm-wrapper">
+		<div class="bm-window">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.bm-wrapper {
+	left: 0;
+	top: 0;
+	position: fixed;
+
+	z-index: 2;
+	width: 100%;
+	height: 100%;
+
+	background: rgba(255, 255, 255, 0.1);
+	backdrop-filter: blur(22px);
+}
+</style>
