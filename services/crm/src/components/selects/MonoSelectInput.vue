9d773fc64bda262fed733b27d70cf7aab021d36b
infanasotku <infanasotku@gmail.com>
2025-01-31 13:25:45 +0700

    Correct animations for mono select

--- services/crm/src/components/selects/MonoSelectInput.vue
+++ services/crm/src/components/selects/MonoSelectInput.vue
@@ -49,24 +49,30 @@ const list = computed(() => {
 			:with-edit-mark="true"
 			:required="props.required"
 		></MaybeDelayInput>
-		<TransitionGroup
-			:css="false"
-			tag="ul"
-			class="msi-list"
-			@before-enter="animations.onBeforeEnter"
-			@enter="animations.onEnter"
-			@leave="animations.onLeave"
-			@pointerdown.prevent
-		>
-			<li
-				class="msi-row"
-				:key="row"
-				v-for="(row, index) in list"
-				:data-index="index"
-			>
-				{{ row }}
-			</li>
-		</TransitionGroup>
+		<Transition name="fade">
+			<div class="msi-list-wrapepr" v-show="list.length">
+				<TransitionGroup
+					:css="false"
+					tag="ul"
+					class="msi-list"
+					@before-enter="animations.onBeforeEnter"
+					@enter="animations.onEnter"
+					@leave="animations.onLeave"
+					@pointerdown.prevent
+				>
+					<li
+						class="msi-row"
+						:key="row"
+						v-for="(row, index) in list"
+						:data-index="index"
+					>
+						<span>
+							{{ row }}
+						</span>
+					</li>
+				</TransitionGroup>
+			</div>
+		</Transition>
 	</div>
 </template>
 
@@ -74,26 +80,64 @@ const list = computed(() => {
 .msi-wrapper {
 	display: flex;
 	flex-direction: column;
+	position: relative;
 
 	.msi-input {
 		width: 100%;
 		height: 48px;
 	}
 
-	.msi-list {
-		display: flex;
-		flex-direction: column;
-
+	.msi-list-wrapepr {
+		position: absolute;
+		top: 48px;
+		left: 0;
 		width: 100%;
-		padding: 0;
-		margin: 0;
 
-		.msi-row {
-			margin-top: 8px;
-			height: 48px;
+		border-radius: 8px;
+		border: 1px solid $stroke-gray;
+		background-color: $main-white;
+		padding: 16px;
+
+		.msi-list {
+			display: flex;
+			flex-direction: column;
+
+			width: 100%;
+			margin: 0;
+			padding: 0;
+			max-height: 86px;
+			overflow-y: scroll;
+
+			.msi-row {
+				display: flex;
+				align-items: center;
+
+				padding: 8px;
+				margin-top: 4px;
+				height: fit-content;
+				width: 100%;
+
+				span {
+					font-family: Wix Madefor Display;
+					font-size: 14px;
+					font-weight: 500;
+					line-height: 17.64px;
+				}
+
+				&:hover {
+					cursor: pointer;
+					background-color: $bg-light-blue;
+				}
+
+				&:first-child {
+					margin-top: 0 !important;
+				}
+
+				transition: background-color 0.25s;
+			}
+
+			transition: margin-top 0.25s;
 		}
-
-		transition: margin-top 0.25s;
 	}
 }
 </style>
--- services/crm/src/components/selects/monoSelectAnimations.ts
+++ services/crm/src/components/selects/monoSelectAnimations.ts
@@ -3,8 +3,8 @@ import gsap from "gsap";
 export function onBeforeEnter(el: any) {
 	el.style.opacity = 0;
 	el.style.height = 0;
-	el.style.paddingTop = 0;
 	el.style.marginTop = 0;
+	el.style.paddingTop = 0;
 	el.style.paddingBottom = 0;
 	el.style.overflow = "hidden";
 }
@@ -12,10 +12,10 @@ export function onBeforeEnter(el: any) {
 export function onEnter(el: any, done: any) {
 	gsap.to(el, {
 		opacity: 1,
-		paddingTop: "15px",
-		paddingBottom: "15px",
-		marginTop: "8px",
-		height: "48px",
+		marginTop: "4px",
+		paddingTop: "8px",
+		paddingBottom: "8px",
+		height: "26px",
 		delay: el.dataset.index * 0.15,
 		onComplete: done,
 	});
