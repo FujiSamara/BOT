8537f8f1878ef69441542db34c53de9e2dce0f61
infanasotku <infanasotku@gmail.com>
2025-02-27 18:23:39 +0700

    Completed security contract

---
+++ common/src/common/auth/security.py
@@ -0,0 +1,45 @@
+from datetime import timedelta
+import jwt
+
+from common.contracts.clients import SecurityClient
+from common.schemas.token import TokenPayload
+from common import now
+
+
+class PyJWTSecurityClient(SecurityClient):
+    algorithm = "RS256"
+
+    def __init__(
+        self,
+        public_key: str,
+        private_key: str | None,
+        *,
+        expires_delta: timedelta | None = None,
+    ):
+        self.expires_delta = expires_delta or timedelta(minutes=15)
+        self._private_key = private_key
+        self._public_key = public_key
+
+    def create_access_token(self, data):
+        if self._private_key is None:
+            raise ValueError("Private key not specified for creating token.")
+        expire = now() + self.expires_delta
+        return jwt.encode(
+            {"exp": expire, **data}, self._private_key, algorithm=self.algorithm
+        )
+
+    def parse_access_token(self, token):
+        try:
+            payload: dict = jwt.decode(
+                token, self._public_key, algorithms=self.algorithm
+            )
+        except Exception:
+            return
+        client_id = payload.get("sub")
+        expire = payload.get("exp")
+        if client_id is None:
+            return
+        if expire is None:
+            return
+        scopes: str = payload.get("scopes", "")
+        return TokenPayload(id=client_id, scopes=scopes.split(), expire=expire)
