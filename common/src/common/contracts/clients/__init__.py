3dfc65032a504ebaf67048f6703be1c92811eb2e
infanasotku <infanasotku@gmail.com>
2025-03-04 16:45:45 +0700

    Removed extra

--- common/src/common/contracts/clients/__init__.py
+++ common/src/common/contracts/clients/__init__.py
@@ -1,7 +1,5 @@
 from common.contracts.clients.security import SecurityClient
 from common.contracts.clients.base import BaseClient
-from common.contracts.clients.remote import RemoteBaseClient
-from common.contracts.clients.message import MessageInClient, MessageOutClient
 
 __all__ = [
     "SecurityClient",
