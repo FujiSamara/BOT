4905991631e6d2585422091648b1a8f4a4a3cbbb
infanasotku <infanasotku@gmail.com>
2025-03-10 23:51:11 +0700

    Completed endpoint for s3 webhook

--- services/file/app/controllers/api/router.py
+++ services/file/app/controllers/api/router.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends, Security
+from fastapi import APIRouter, Depends, HTTPException, Request, Security, status
 from dependency_injector.wiring import Provide, inject
+import json
 
 from common.schemas.client_credential import ClientCredentials
 from app.container import Container
 from app.contracts.services import FileService
-from app.schemas.file import FileInSchema
+from app.schemas.file import FileInSchema, FileConfirmSchema
 
 from app.controllers.api.dependencies import Authorization
 from app.infra.config.scopes import Scopes
@@ -26,12 +27,16 @@ async def create_put_link(
         scopes=[Scopes.FileRead.value],
     ),
 ) -> str:
-    return await file_service.create_put_link(file)
+    """Creates presigned url for putting file with specified meta."""
+    try:
+        link = await file_service.create_put_link(file)
+    except KeyError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    return link
 
 
 @router.get(
     "/{id}",
-    description="Creates presigned url for getting file with specified meta.",
     response_description="Created url",
 )
 @inject
@@ -43,4 +48,38 @@ async def create_get_link(
         scopes=[Scopes.FileRead.value],
     ),
 ) -> str:
-    return await file_service.create_get_link(id)
+    """Creates presigned url for getting file with specified meta."""
+    try:
+        link = await file_service.create_get_link(id)
+    except KeyError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    return link
+
+
+@router.post("/s3_webhook")
+@inject
+async def s3_webhook(
+    request: Request,
+    file_service: FileService = Depends(Provide[Container.file_service]),
+):
+    """Webhook specified for vkcloud s3 events."""
+    body = await request.body()
+    data = json.loads(body)
+    records: list = data["Records"]
+
+    for record in records:
+        event_name = record["eventName"]
+        if event_name != "s3:ObjectCreated:Put":
+            continue
+
+        s3 = record["s3"]
+        bucket: str = s3["bucket"]["name"]
+        key = s3["object"]["key"]
+        size = s3["object"]["size"]
+
+        file_confirm = FileConfirmSchema(key=key, bucket=bucket, size=size)
+
+        try:
+            await file_service.confirm_putting(file_confirm)
+        except (ValueError, KeyError) as e:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
