c573ac2ee4af721d204cb520c68984967d8e31d1
infanasotku <infanasotku@gmail.com>
2025-03-10 13:06:42 +0700

    Remove keyboard interrupt error

--- services/file/manage.py
+++ services/file/manage.py
@@ -10,15 +10,18 @@ from app import app
 def run():
     settings = generate(Settings, logger)
 
-    uvicorn.run(
-        app=app,
-        host=settings.host,
-        port=settings.port,
-        log_config=config,
-        ssl_keyfile=settings.ssl_keyfile,
-        ssl_certfile=settings.ssl_certfile,
-        root_path=settings.root_path,
-    )
+    try:
+        uvicorn.run(
+            app=app,
+            host=settings.host,
+            port=settings.port,
+            log_config=config,
+            ssl_keyfile=settings.ssl_keyfile,
+            ssl_certfile=settings.ssl_certfile,
+            root_path=settings.root_path,
+        )
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == "__main__":
