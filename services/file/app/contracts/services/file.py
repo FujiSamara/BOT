60734b51394cb59cfbffdbbd590572ac3a9ac2d2
infanasotku <infanasotku@gmail.com>
2025-03-10 11:46:32 +0700

    Defined contract for file service

---
+++ services/file/app/contracts/services/file.py
@@ -0,0 +1,29 @@
+from abc import abstractmethod
+
+from common.contracts.services import BaseService
+from app.schemas.file import FileInSchema, FileConfirmSchema
+
+
+class FileService(BaseService):
+    @abstractmethod
+    async def create_put_link(self, file: FileInSchema) -> str:
+        """Create presigned url for putting file.
+        Create metadata for file with `confirmed=False`.
+        Raises:
+            KeyError: if file with specified `FileInSchema.key` already exists in all buckets.
+        Returns:
+            Created link.
+        """
+
+    @abstractmethod
+    async def create_get_link(self, key: str) -> str:
+        """Create presigned url for getting file.
+        Raises:
+            KeyError: if file with specified `key` not exists.
+        Returns:
+            Created link.
+        """
+
+    @abstractmethod
+    async def confirm_putting(self, file_confirm: FileConfirmSchema):
+        """Confirms putting file into s3 storage."""
