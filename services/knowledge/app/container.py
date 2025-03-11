6f29ff44559ec245c3a98513564c9c24640af622
infanasotku <infanasotku@gmail.com>
2025-03-11 16:42:26 +0700

    Added auth client to container

--- services/knowledge/app/container.py
+++ services/knowledge/app/container.py
@@ -4,6 +4,7 @@ from dependency_injector import providers, containers
 from common.containers.auth import LocalAuthContainer
 from common.containers.postgres import PostgresContainer
 from common.auth import LocalAuthService
+from common.http.auth import AuthHTTPClient
 
 
 class Container(containers.DeclarativeContainer):
@@ -13,6 +14,15 @@ class Container(containers.DeclarativeContainer):
     postgres_container = providers.Container(PostgresContainer, config=config)
     auth_container = providers.Container(LocalAuthContainer, config=config)
 
-    auth_service = providers.Resource(
+    auth_service = providers.Factory(
         LocalAuthService, auth_container.container.security_client
+    )
+
+    auth_client = providers.Factory(
+        AuthHTTPClient,
+        auth_service,
+        auth_url=config.auth_url,
+        client_id=config.client_id,
+        client_secret=config.client_secret,
+        with_ssl=config.with_auth_ssl,
     )
