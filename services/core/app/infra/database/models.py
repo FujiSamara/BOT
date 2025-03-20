cbb42bcfae48d3f85760943cbe793eebf2cb1085
Bortnikov Mikhail <mbortnikov@infotech-cloudsolutions.com>
2025-03-20 12:30:27 +0300

    ttk models

--- services/core/app/infra/database/models.py
+++ services/core/app/infra/database/models.py
@@ -1176,7 +1176,9 @@ class WorkerFingerprint(Base):
     cell_number: Mapped[int] = mapped_column(nullable=True)
     rfid_card: Mapped[str] = mapped_column(nullable=True)
 
-    worker: Mapped["Worker"] = relationship("Worker", foreign_keys=[worker_id])
+    worker: Mapped["Worker"] = relationship(
+        "Worker", back_populates="workers_fingerprints", foreign_keys=[worker_id]
+    )
 
     department: Mapped["Department"] = relationship(
         "Department", foreign_keys=[department_id]
@@ -1214,3 +1216,67 @@ class AuthClientScope(Base):
     )
 
     name: Mapped[str] = mapped_column(nullable=False)
+
+
+class TTKDish(Base):
+    __tablename__ = "dishes"
+
+    iiko_uuid: Mapped[UUID] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    code: Mapped[str] = mapped_column(nullable=False)
+    num: Mapped[str] = mapped_column(nullable=False)
+    deleted: Mapped[bool] = mapped_column(nullable=False)
+    weight: Mapped[float] = mapped_column(nullable=False)
+    capacity: Mapped[float] = mapped_column(nullable=False)
+    price: Mapped[float] = mapped_column(nullable=False)
+
+
+class TTKDishModifier(Base):
+    __tablename__ = "modifiers"
+
+    iiko_uuid: Mapped[UUID] = mapped_column(nullable=False)
+    dish_id: Mapped[int] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    code: Mapped[str] = mapped_column(nullable=False)
+    num: Mapped[str] = mapped_column(nullable=False)
+    deleted: Mapped[bool] = mapped_column(nullable=False)
+    weight: Mapped[float] = mapped_column(nullable=False)
+    capacity: Mapped[float] = mapped_column(nullable=False)
+    price: Mapped[float] = mapped_column(nullable=False)
+    defaultAmount: Mapped[float] = mapped_column(nullable=False)
+    minimumAmount: Mapped[float] = mapped_column(nullable=False)
+    maximumAmount: Mapped[float] = mapped_column(nullable=False)
+
+    dish: Mapped["TTKDish"] = relationship(
+        "TTKDish", back_populates="modifiers", foreign_keys=[dish_id]
+    )
+
+
+class TTKProduct(Base):
+    __tablename__ = "products"
+
+    iiko_uuid: Mapped[UUID] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    code: Mapped[str] = mapped_column(nullable=False)
+    num: Mapped[str] = mapped_column(nullable=False)
+    deleted: Mapped[bool] = mapped_column(nullable=False)
+
+
+class AssemblyChart(Base):
+    __tablename__ = "assembly_charts"
+
+    iiko_uuid: Mapped[UUID] = mapped_column(nullable=False)
+    modifier_id: Mapped[int] = mapped_column(nullable=False)
+    product_id: Mapped[int] = mapped_column(nullable=False)
+    weight: Mapped[float] = mapped_column(nullable=False)
+    amount: Mapped[float] = mapped_column(nullable=False)
+
+    modifier: Mapped["TTKDishModifier"] = relationship(
+        "TTKDishModifier", back_populates="assembly_charts", foreign_keys=[modifier_id]
+    )
+    product: Mapped["TTKProduct"] = relationship(
+        "TTKProduct", back_populates="assembly_charts", foreign_keys=[product_id]
+    )
