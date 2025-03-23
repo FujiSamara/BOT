fdfff81c24d1eac4492dced6b9ea68ea59466b6e
infanasotku <infanasotku@gmail.com>
2025-03-23 22:20:13 +0700

    Added schemas for division

--- services/knowledge/app/schemas/division.py
+++ services/knowledge/app/schemas/division.py
@@ -1,7 +1,25 @@
+from enum import Enum
 from common.schemas.base import BaseSchema
+
+
+class SubdivisionType(Enum):
+    division = 1
+    # Card types
+    dish = 2
+    business = 3
+
+
+class SubdivisionSchema(BaseSchema):
+    id: int
+    name: int
+    type: SubdivisionType
 
 
 class DivisionSchema(BaseSchema):
     id: int
     name: str
     path: str
+
+
+class DivisionOutSchema(DivisionSchema):
+    subdivisions: list[SubdivisionSchema]
