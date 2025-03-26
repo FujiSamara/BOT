0a0647950eb7c9a41dae3cb3f532b0c458439f29
infanasotku <infanasotku@gmail.com>
2025-03-26 11:42:48 +0700

    Added contract for dish service

--- services/knowledge/app/contracts/services/__init__.py
+++ services/knowledge/app/contracts/services/__init__.py
@@ -1,4 +1,5 @@
 from app.contracts.services.division import DivisionService
 from app.contracts.services.card import CardService
+from app.contracts.services.dish import DishService
 
-__all__ = ["DivisionService", "CardService"]
+__all__ = ["DivisionService", "CardService", "DishService"]
---
+++ services/knowledge/app/contracts/services/dish.py
@@ -0,0 +1,15 @@
+from abc import abstractmethod
+
+from common.contracts.services import BaseService
+
+from app.schemas.dish import DishSchema, DishModifierSchema
+
+
+class DishService(BaseService):
+    @abstractmethod
+    async def get_dish_by_id(self, id: int) -> DishSchema | None:
+        pass
+
+    @abstractmethod
+    async def get_dish_modifiers(self, dish_id: int) -> list[DishModifierSchema]:
+        pass
