97c533be0b38336678a5f769b9aed8adb4486060
infanasotku <infanasotku@gmail.com>
2025-04-01 21:56:01 +0700

    Added metadata to output files

--- common/src/common/schemas/file.py
+++ common/src/common/schemas/file.py
@@ -1,6 +1,13 @@
+from datetime import datetime
 from common.schemas.base import BaseSchema
 
 
-class FileLinkSchema(BaseSchema):
+class FileMetaSchema(BaseSchema):
     id: int
+    name: str
+    size: float
+    created: datetime
+
+
+class FileLinkSchema(FileMetaSchema):
     url: str
