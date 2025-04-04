dc39ed237b243ab9ba71158e09ee8447cd081f66
infanasotku <infanasotku@gmail.com>
2025-04-04 16:42:28 +0700

    Added ignoring exceptions

--- common/src/common/http/retry.py
+++ common/src/common/http/retry.py
@@ -1,11 +1,15 @@
 from typing import Callable
 
 
-def retry(times: int = 5):
+def retry(times: int = 5, ignored: list[Exception] | None = None):
     """Decorates callable with retrying it `times` times.
 
     - Note Retries work only for python `Exception`.
+    Args:
+        ignored: list of exception which throw without retrying.
     """
+    if ignored is None:
+        ignored = []
 
     def wrap(func: Callable):
         async def repeat_request(*args, **kwargs):
@@ -14,6 +18,8 @@ def retry(times: int = 5):
                 try:
                     times_left -= 1
                     return await func(*args, **kwargs)
+                except ignored:
+                    raise
                 except Exception:
                     if not times_left:
                         raise
